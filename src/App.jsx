1f5e8f4f58992f9f41bc8456ae3f4697c6458f62
sockki <poohct327@gmail.com>
2024-11-19 02:58:48 +0900

    컴포넌트 사용 가능하게 변경

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
-function App() {
+function Header() {
+  return (
+    <header>
+      <h1>My React Clone</h1>
+    </header>
+  );
+}
+
+function Content() {
+  return (
+    <main>
+      <p>This is a sample content.</p>
+    </main>
+  );
+}
+
+
+export default function App() {
   return (
     <div id="app">
-      <h1>Hello, React Clone!</h1>
+      <Header />
+      <div>
+        {[1,2,3,4,5].map(v => <Content />)}
+      </div>
     </div>
   );
 }
-export default App;
