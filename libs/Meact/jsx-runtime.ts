0bbe2c166f049242bfca99bc4a1b215038db1e14
sockki <poohct327@gmail.com>
2024-11-25 14:23:54 +0900

    pettier 적용

--- libs/Meact/jsx-runtime.ts
+++ libs/Meact/jsx-runtime.ts
@@ -1,4 +1,4 @@
-type DomNode = HTMLElement
+type DomNode = HTMLElement;
 
 type JsxsConfig = {
   [key: string]: Element[];
@@ -66,7 +66,9 @@ export const jsx = (
 };
 
 export const render = (meactNode: Element, domNode: DomNode) => {
-  const dom = document.createElement(meactNode.type === Fragment ? "div" : meactNode.type);
+  const dom = document.createElement(
+    meactNode.type === Fragment ? "div" : meactNode.type
+  );
 
   Object.keys(meactNode.props).forEach((key) => {
     if (key !== "children" && meactNode.props[key]) {
@@ -77,17 +79,18 @@ export const render = (meactNode: Element, domNode: DomNode) => {
   switch (typeof meactNode.props.children) {
     case "string":
     case "number":
-      dom.textContent = meactNode.props.children.toString()
+      dom.textContent = meactNode.props.children.toString();
       break;
 
     case "object":
-      if(Array.isArray(meactNode.props.children)){
-        meactNode.props.children.forEach((child:Element) => render(child, dom))
+      if (Array.isArray(meactNode.props.children)) {
+        meactNode.props.children.forEach((child: Element) =>
+          render(child, dom)
+        );
+      } else {
+        render(meactNode.props.children as Element, dom);
       }
-      else {
-        render(meactNode.props.children as Element, dom)
-      }
-      break
+      break;
   }
   domNode.appendChild(dom);
 };
