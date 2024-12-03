47e66913efd2d4bd9a9817af168ef424e4366921
sockki <poohct327@gmail.com>
2024-12-03 21:43:14 +0900

    Fix: useState 수정

--- libs/Meact/useState.ts
+++ libs/Meact/useState.ts
@@ -5,6 +5,8 @@ type UseStateReturn<T> = [T, (newState: T) => void];
 let stateIndex = 0; // 현재 렌더링에서 참조할 상태의 인덱스
 const stateArr: unknown[] = []; // 상태를 저장하는 배열
 
+let isRenderingScheduled = false;
+
 export const useState = <T>(initState: T): UseStateReturn<T> => {
   // 현재 상태 배열에서 값 가져오기, 없으면 초기화
   if (stateArr[stateIndex] === undefined) {
@@ -17,10 +19,15 @@ export const useState = <T>(initState: T): UseStateReturn<T> => {
 
   // 상태 업데이트 함수
   const setState = (newState: T) => {
-    
     stateArr[currentIndex] = newState; // 해당 인덱스의 상태를 업데이트
-    stateIndex = 0; // 렌더링 시작 시 초기화
-    rendering(); // 렌더링 트리거
+    if (!isRenderingScheduled) {
+      isRenderingScheduled = true;
+      setTimeout(() => { // 비동기로 렌더링 처리
+        isRenderingScheduled = false;
+        stateIndex = 0;
+        rendering(); // 렌더링 트리거
+      }, 0);
+    }
   };
 
   // 다음 상태 참조를 위해 인덱스 증가
@@ -28,7 +35,6 @@ export const useState = <T>(initState: T): UseStateReturn<T> => {
   return [currentState, setState];
 };
 
-
 export const resetStateIndex = () => {
-  stateIndex = 0; 
+  stateIndex = 0;
 };
